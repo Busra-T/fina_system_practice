f44251b7bac1ac01a2db4d18ca66a1d472e0fabb
Busra_Cydeo <juliebt2012@gmail.com>
2023-03-02 01:53:25 -0500

    cukes updated

--- src/test/java/com/fina/runners/CukesRunner.java
+++ src/test/java/com/fina/runners/CukesRunner.java
@@ -12,6 +12,7 @@ import org.junit.runner.RunWith;
                 "json:target/cucumber.json",
                 "pretty",
                 "html:target/cucumber-reports.html",
+                "junit:target/junit/junit-report.xml",
                 "rerun:target/rerun.txt",
                 "me.jvt.cucumber.report.PrettyReports:target/cucumber"
         },
