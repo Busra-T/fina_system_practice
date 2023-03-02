779e03544e0d4ff6d5e7e25265941bbd27d92673
Busra-T <110192617+Busra-T@users.noreply.github.com>
2023-03-02 01:44:02 -0500

    Update CukesRunner.java

--- src/test/java/com/fina/runners/CukesRunner.java
+++ src/test/java/com/fina/runners/CukesRunner.java
@@ -17,7 +17,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/fina/step_definitions",
         dryRun = false,
-        tags = "@smoke",
+        tags = "@wip",
         publish = true
      //   monochrome = false,
 
